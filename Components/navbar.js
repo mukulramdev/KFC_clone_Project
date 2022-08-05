caa3651bae514d6e012bfdc50dcfc473379a9d4b
vikasviplav <vikashvipplav@gmail.com>
2022-08-05 16:15:01 +0530

    new commit

---
+++ Components/navbar.js
@@ -0,0 +1,20 @@
+function navbar(){
+    return `<div id="nav">
+    <div id="firstHalf">
+        <img src="https://online.kfc.co.in/static/media/kfcLogo.492728c6.svg">
+        <p>Menu</p>
+        <p>Deals</p>
+    </div>
+    <div id="secondHalf">
+        <div>
+            <img src="https://images.ctfassets.net/wtodlh47qxpt/6bJdGLRkksNvWP4LI9ZiFF/cb89d6393492fd093e0f99980abfa39e/Account_Icon.svg">
+            <h4>Sign In</h4>
+        </div>    
+        <p id="rupee">&#8377; 0</p>
+        <img class="bucket" src="https://images.ctfassets.net/wtodlh47qxpt/6qtBVFuno7pdwOQ9RIvYm9/d13e9b7242980972cf49beddde2cc295/bucket_cart_icon.svg">
+    </div>
+</div>`
+};
+
+
+export default navbar 
---
+++ deals.js
@@ -0,0 +1,8 @@
+import navbar from './Components/navbar.js'
+let nav = document.getElementById("header")
+nav.innerHTML = navbar()
+import footer from './Components/footer.js'
+let foot = document.getElementById("footer")
+foot.innerHTML = footer()
+console.log(foot)
+
