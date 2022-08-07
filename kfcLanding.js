cc1fde8e1594c3ffe5516f43ed428fa129319c23
Mukul <ramdevmukul800@gmail.com>
2022-08-07 13:17:33 +0530

    updated

---
+++ kfcLanding.js
@@ -0,0 +1,5 @@
+import {footer,navbar}from "./common.js "
+var footery=document.getElementById("footer")
+footery.innerHTML=footer()
+var navy=document.getElementById("navbar")
+navy.innerHTML=navbar()
